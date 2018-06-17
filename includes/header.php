a8a98e77af693687cd9d3b183e7859791ac16338
andrescanm <andres.co@hotmail.com>
2018-06-16 19:06:53 -0500

    Se agrega Bootstrap CDN

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-	   <meta charset="utf-8">
-       <title>PSP SITP POLI</title>
-      <link href="css/style.css" media="screen" rel="stylesheet">
-	  	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-</head> 
-	
+	<head>
+		<meta charset="utf-8">
+    	<title>PSP SITP POLI</title>
+    	<link href="css/style.css" media="screen" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
--- index.php
+++ index.php
@@ -3,12 +3,12 @@ session_start();
 session_unset();
 ?>
 
-<?php  
-include("includes/header.php"); 
+<?php
+include("includes/header.php");
 ?>
 <div class="container mlogin">
             <div id="login">
-    <img src="imagenes/sitp.jpg" width="310" height="210" >
+    <img src="imagenes/sitp.jpg" width="310 px" height="auto" >
 <form name="loginform" id="loginform" action="includes/validaexisteusuario.php" method="POST">
     <p>
         <label for="user_login">Nombre De Usuario<br />
@@ -19,12 +19,11 @@ include("includes/header.php");
         <input type="password" name="password" id="password" class="input" value="" size="8" required></label>
     </p>
         <p class="submit">
-        <input type="submit" name="login" class="button" value="Ingrese Banco" />
+        <input type="submit" name="login" class="button" value="Ingrese" />
     </p>
         <p class="regtext">¿No estas registrado? <a href="registro.php" >Registrate Aquí</a></p>
 </form>
     </div>
     </div>
 <?php if (!empty($message)) {echo "<p class=\"error\">" . "". $message . "</p>";} ?>
-	
-	
+
